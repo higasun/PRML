af90c9fe8bfcae3104bcbb665b941a9d86fa80a8
Taichi Higasa <albatross@asagi.waseda.jp>
2022-07-16 09:12:32 +0900

    add logistic regression

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from .fisher import *
 from .naive_fisher import *
+from .logistic_regression import *
--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -1,2 +1,37 @@
 import numpy as np
+from scipy import linalg
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+THRESMIN = 1e-10
+
+class LogisticRegression():
+    def __init__(self, tol=0.001, max_iter=3, random_seed=0) -> None:
+        self.w = None
+        self.tol = tol
+        self.random_state = np.random.RandomState(random_seed)
+        self.max_iter = max_iter
+        pass
+
+    def fit(self, X: np.array, t: np.array) -> None:
+        self.w  = self.random_state.randn(X.shape[0] + 1)
+        Xtil =  np.c_[np.ones(X.shape[0]), X]
+        diff = np.inf
+        iteration = 0
+
+        while diff > self.tol and iteration < self.max_iter:
+            y = sigmoid(np.dot(Xtil, self.w))
+            r = np.clip(y * (1 - y), THRESMIN, np.inf)
+            XR = Xtil.T * r
+            XRX = np.dot(XR, r)
+            w_prev = self.w
+            b = np.dot(XR, np.dot(Xtil.T, self.w_) - 1/r * (y - t))
+            self.w = linalg.solve(XRX, b)
+            diff = abs(self.w - w_prev).mean()
+            iteration += 1
+
+    def predict(self, X):
+        Xtil = np.c_[np.ones(X.shape[0]), X]
+        y = sigmoid(np.dot(Xtil, self.w))
+        return np.where(y > 0.5, 1, 0)
