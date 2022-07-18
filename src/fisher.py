f6c1860334df2cfedb77f476878df83537fe6458
Taichi Higasa <albatross@asagi.waseda.jp>
2022-07-18 10:19:56 +0900

    modified

--- src/fisher.py
+++ src/fisher.py
@@ -23,8 +23,8 @@ def plot(cls1, cls2, line1, line2):
 
 if __name__ == '__main__':
     
-    #テスト用2次元データ
-    cov = [[4,3],[3,4]] # 共分散
+    # two-dimensional data
+    cov = [[4,3],[3,4]] # Covariance
     cls1 = np.random.multivariate_normal([-1.8, 1.8], cov, 100)
     cls2 = np.random.multivariate_normal([1.8, -1.8], cov, 100)
 
